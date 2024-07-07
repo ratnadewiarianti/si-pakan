34b88aef72bc851e8229fe967d5ae21e294a0165
sulungwm <sulungwayto@gmail.com>
2024-07-08 00:48:58 +0700

    revisi akhir

--- app/Models/DetailPenatausahaanModel.php
+++ app/Models/DetailPenatausahaanModel.php
@@ -116,6 +116,7 @@ public function getDetail($id)
         ->first();
     }
 
+   
     public function updateStatusVerifikasi($id, $status)
 {
     $data = [
--- app/Models/JenisModel.php
+++ app/Models/JenisModel.php
@@ -18,4 +18,18 @@ class JenisModel extends Model
         ->join('akun', 'akun.id = kelompok.id_akun')
         ->findAll();
     }
+
+    public function getUraianJenis($id)
+    {
+        $row = $this->select('jenis.uraian_jenis')
+        ->join('objek', 'jenis.id = objek.id_jenis')
+        ->join('rincian_objek', 'objek.id = rincian_objek.id_objek')
+        ->join('sub_rincian_objek', 'rincian_objek.id = sub_rincian_objek.id_rincian_objek')
+        ->join('detail_dpa', 'sub_rincian_objek.id = detail_dpa.id_rekening')
+        ->join('detail_penatausahaan as dp', 'detail_dpa.id = dp.id_detail_dpa')
+        ->where('dp.id', $id)
+            ->first(); // Use first() to get a single row
+
+        return $row ? $row['uraian_jenis'] : ''; // Return the value or an empty string if no row found
+    }
 }
