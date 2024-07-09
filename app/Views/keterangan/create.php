0526f62a17727a5c4e9ecbb3ed350b51fe08289d
ratnadewiarianti <ratnadewiarianti2906@gmail.com>
2024-07-09 08:58:42 +0800

    keterangan penatausahaan

--- app/Views/keterangan/create.php
+++ app/Views/keterangan/create.php
@@ -9,31 +9,32 @@
                         <h4 class="card-title">Tambah Keterangan Detail Penatausahaan</h4>
                         <form class="forms-sample" action="/keterangan/store" method="post">
 
-                            <input type="hidden" name="id_detail_penatausahaan" value="<?= service('uri')->getSegment(3); ?>" class="form-control" required>
+                            <input type="hidden" name="id_detail_penatausahaan"
+                                value="<?= service('uri')->getSegment(3); ?>" class="form-control" required>
 
                             <!-- 'id_dpa','id_subkegiatan','id_rekening','jumlah','jumlah_perubahan' -->
-
-                         
                             <div class="form-group">
-                                <label>Keperluan</label>
-                                <input type="text" name="keperluan" required class="form-control">
-                            </div>
-                            <div class="form-group">
-                                <label>Harga</label>
-                                <div class="input-group">
-                                    <div class="input-group-prepend">
-                                        <span class="input-group-text">Rp.</span>
-                                    </div>
-                                    <input type="number" name="harga" class="form-control" required>
-                                </div>
+                                <label>DPA Subkegiatan</label>
+                                <select class="form-control" name="id_dpa_subkegiatan" id="id_dpa_subkegiatan" required>
+                                    <option selected disabled>-</option>
+                                    <?php foreach ($detaildpasubkegiatan as $key) : ?>
+                                    <option value="<?= $key['id']; ?>" data-jumlah="<?= $key['jumlah']; ?>">
+                                        <?= $key['uraian']; ?>, <?= $key['koefisien']; ?> <?= $key['satuan']; ?>, Harga:
+                                        Rp. <?= $key['harga']; ?>, Jumlah: <?= $key['jumlah']; ?></option>
+                                    <?php endforeach; ?>
+                                </select>
                             </div>
                             <div class="form-group">
                                 <label>Jumlah</label>
-                                <input type="number" name="jumlah" required class="form-control">
+                                <input type="number" name="jumlah" id="jumlah" required class="form-control">
                             </div>
+
+
+
                             <button type="submit" class="btn btn-success mr-2">Simpan</button>
                             <!-- <button class="btn btn-light">Batal</button> -->
-                            <a href="/keterangan/show/<?= $detailpenatausahaan['id'] ?>" class="btn btn-danger">Batal</a>
+                            <a href="/keterangan/show/<?= $detailpenatausahaan['id'] ?>"
+                                class="btn btn-danger">Batal</a>
                         </form>
                     </div>
                 </div>
@@ -55,4 +56,12 @@
 
 <?= $this->section('javascript') ?>
 <!--  script src -->
+<script>
+    document.getElementById('id_dpa_subkegiatan').addEventListener('change', function () {
+        var selectedOption = this.options[this.selectedIndex];
+        var maxJumlah = selectedOption.getAttribute('data-jumlah');
+        var jumlahInput = document.getElementById('jumlah');
+        jumlahInput.max = maxJumlah;
+    });
+</script>
 <?= $this->endSection() ?>
